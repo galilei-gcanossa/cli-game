271c8c57cb9c0880ec78578b29f5fa24253ac5b6
Gianmaria Canossa <gianmaria.canossa@galileiostiglia.edu.it>
2022-02-17 09:33:20 +0100

    feat: base clear and print functions

--- main.c
+++ main.c
@@ -12,11 +12,38 @@ int main(int argc, char** argv){
   return 0;
 }
 
+void fillWorld(char* world, int width, int height, char fillChar);
+void printWorld(char* world, int width, int height);
+
 void init(){
 
 }
 
 void loop(GameInput* input){
 
-  printf("ciao %c\n", input == NULL ? '\0' : input->keyPressed);
+  int width=50;
+  int height=10;
+  char* world = (char*)malloc(sizeof(char)*width*height);
+
+  fillWorld(world, width, height, 'a');
+  printWorld(world, width, height);
+
+  free(world);
+}
+
+void fillWorld(char* world, int width, int height, char fillChar){
+  for(int y=0;y<height;y++){
+    for(int x=0;x<width;x++){
+      *(world + x + y*width) = fillChar;
+    }
+  }
+}
+
+void printWorld(char* world, int width, int height){
+  for(int y=0;y<height;y++){
+    for(int x=0;x<width;x++){
+      printf("%c", *(world + x + y*width));
+    }
+    printf("\n");
+  }
 }
