5067e63019e6694dd37088cdfe1915cf3020cfc3
Gianmaria Canossa <gianmaria.canossa@galileiostiglia.edu.it>
2022-02-24 17:17:11 +0100

    feat: added menu

--- main.c
+++ main.c
@@ -15,6 +15,8 @@ int main(int argc, char** argv){
 void fillWorld(char* world, int width, int height, char fillChar);
 void printWorld(char* world, int width, int height);
 void drawShip(char* world, int width, int height, int x, int y);
+void drawString(char* world, int width, int height, int x, int y, char* string);
+void drawMenu(char* world, int width, int height, int x, int y, int selected);
 
 void init(){
 
@@ -22,6 +24,8 @@ void init(){
 
 void loop(GameInput* input){
 
+  static int screen = 0; //0 menu, 1 game 
+  static int selectedMenu = 0;
   static int shipX = 0;
   static int shipY = 0;
 
@@ -29,22 +33,46 @@ void loop(GameInput* input){
   int height=10;
   char* world = (char*)malloc(sizeof(char)*width*height);
 
-  fillWorld(world, width, height, ' ');
-  if(input!=NULL){
-    if(input->keyPressed=='d' && shipX < width)
-      shipX++;
-      
-    if(input->keyPressed=='a' && shipX > 0)
-      shipX--;
+  if(screen == 0){
+    fillWorld(world, width, height, ' ');
+    if(input!=NULL){
+      if(input->keyPressed=='s')
+        selectedMenu = (selectedMenu+1)%2;
+        
+      if(input->keyPressed=='w')
+        selectedMenu = (selectedMenu-1)%2;
+        
+      if(input->keyPressed=='\n' || input->keyPressed=='\r'){
+        if(selectedMenu == 0)
+          screen = 1;
 
-    if(input->keyPressed=='s' && shipY < height)
-      shipY++;
-      
-    if(input->keyPressed=='w' && shipY > 0)
-      shipY--;
+        if(selectedMenu == 1){
+          exit(0);
+        }
+      }
+    }
+    drawMenu(world, width, height, 0, 0, selectedMenu);
+
+    printWorld(world, width, height);
   }
-  drawShip(world, width, height, shipX, shipY);
-  printWorld(world, width, height);
+  else {
+    fillWorld(world, width, height, ' ');
+    if(input!=NULL){
+      if(input->keyPressed=='d' && shipX < width)
+        shipX++;
+        
+      if(input->keyPressed=='a' && shipX > 0)
+        shipX--;
+
+      if(input->keyPressed=='s' && shipY < height)
+        shipY++;
+        
+      if(input->keyPressed=='w' && shipY > 0)
+        shipY--;
+    }
+    drawShip(world, width, height, shipX, shipY);
+    printWorld(world, width, height);
+  }
 
   free(world);
 }
@@ -75,4 +103,21 @@ void drawShip(char* world, int width, int height, int x, int y){
   *(world + (x + 0) + (y + 0)*width) = 'V';
   *(world + (x + 0) + (y + 1)*width) = 'V';
   *(world + (x + 1) + (y + 1)*width) = 'V';
+}
+
+void drawString(char* world, int width, int height, int x, int y, char* s){
+  for(int i=0; s[i]!='\0';i++)
+    *(world + (x + i) + y*width) = s[i];
+}
+
+void drawMenu(char* world, int width, int height, int x, int y, int selected){
+  drawString(world, width, height, x+10, y+3, "START");
+  drawString(world, width, height, x+10, y+5, "EXIT");
+
+  if(selected == 0){
+    drawString(world, width, height, x+8, y+3, "*");
+  }
+  else {
+    drawString(world, width, height, x+8, y+5, "*");
+  }
 }
